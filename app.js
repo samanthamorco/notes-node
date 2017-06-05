4579f402b41fe5a546df545eed4e8df41bd4a471
Samantha Morco <samanthamorco@gmail.com>
2017-06-05 14:36:29 -0400

    messing around with node

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log('Starting app...');
+
+const fs = require('fs');
+const os = require('os');
+const _ = require('lodash');
+const notes = require('./notes.js');
+
+var user = os.userInfo();
+
+// fs.appendFile('greetings.txt', `\n Hello ${user.username}! You are ${notes.age}.`, function (err) {
+//   if (err) {
+//     console.log('Unable to write to file!');
+//   }
+// });
+
+// var res = notes.addNote();
+// console.log(res);
+// console.log('Result:', notes.add(9,-2));
+console.log(_.isString(true));
+console.log(_.isString('Sami'));
